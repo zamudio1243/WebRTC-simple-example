778f33a8c340dc8a586d67dbefed18292f105e19
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-08-24 14:23:40 -0500

    :tada: Primera versión del demo de WebRTC

---
+++ webrtc-demo/main.js
@@ -0,0 +1,168 @@
+import './style.css'
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6gxhINyjRetBOkPsTwuF1Jcm7veQJ02Y",
+  authDomain: "pair-programming-7ea55.firebaseapp.com",
+  projectId: "pair-programming-7ea55",
+  storageBucket: "pair-programming-7ea55.appspot.com",
+  messagingSenderId: "864529477380",
+  appId: "1:864529477380:web:d66ace4b304eee3ac02a51",
+  measurementId: "G-V7ZRHWMHPP"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+// Glonal State
+let pc = new RTCPeerConnection(servers);
+let localStream = null; // Your webcam
+let remoteSteam = null; // Your friend webcam
+
+//HTML elements
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
+
+// 1.Stepup media sources
+
+
+webcamButton.onclick = async () => {
+  // Solicitara inicar la camara en el navegador
+  // Creando nuestro stream de datos de camara y audio
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+
+
+  // Ininicalización del stream a recibir, en este momento es vacio
+  remoteStream = new MediaStream();
+
+  // Push tracks from local stream to peer connection
+  // Push audio/video to the peer connection (pc: RTCPeerConnection)
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  // Pull audio/video to the peer connection (pc: RTCPeerConnection)
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+
+  // Asignacion de los streams a su componente correspondiente
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+};
+
+// 2. Create an offer
+callButton.onclick = async () => {
+  // Reference Firestore collections for signaling
+  const callDoc = firestore.collection('calls').doc();
+  const offerCandidates = callDoc.collection('offerCandidates');
+  const answerCandidates = callDoc.collection('answerCandidates');
+
+  callInput.value = callDoc.id;
+
+  // Get candidates for caller, save to db
+  // Listener for candidates
+  // Setup listener before setLocalDescription
+  pc.onicecandidate = (event) => {
+    event.candidate && offerCandidates.add(event.candidate.toJSON());
+  };
+
+  // Create offer
+  const offerDescription = await pc.createOffer();
+  await pc.setLocalDescription(offerDescription);
+
+  //Protocolo de la oferta
+  const offer = {
+    sdp: offerDescription.sdp, //Session descripcion
+    type: offerDescription.type,
+  };
+
+
+  //Se coloca la oferta
+  await callDoc.set({ offer });
+
+  // Listen for remote answer
+  callDoc.onSnapshot((snapshot) => {
+    const data = snapshot.data();
+    if (!pc.currentRemoteDescription && data?.answer) {
+      const answerDescription = new RTCSessionDescription(data.answer);
+      pc.setRemoteDescription(answerDescription);
+    }
+  });
+
+  // When answered, add candidate to peer connection
+  answerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        const candidate = new RTCIceCandidate(change.doc.data());
+        pc.addIceCandidate(candidate);
+      }
+    });
+  });
+
+  hangupButton.disabled = false;
+};
+
+// 3. Answer the call with the unique ID
+answerButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = firestore.collection('calls').doc(callId); // Hace match con la OFERTA
+  const answerCandidates = callDoc.collection('answerCandidates');
+  const offerCandidates = callDoc.collection('offerCandidates');
+
+  pc.onicecandidate = (event) => {
+    event.candidate && answerCandidates.add(event.candidate.toJSON());
+  };
+
+  const callData = (await callDoc.get()).data();
+
+  const offerDescription = callData.offer;
+  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+  const answerDescription = await pc.createAnswer();
+  await pc.setLocalDescription(answerDescription);
+
+  const answer = {
+    type: answerDescription.type,
+    sdp: answerDescription.sdp,
+  };
+
+  await callDoc.update({ answer });
+
+  offerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      console.log(change);
+      if (change.type === 'added') {
+        let data = change.doc.data();
+        pc.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+};
